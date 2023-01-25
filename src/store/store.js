33205a647c7d0361a2174834e258c14e1178e177
ninochka20102 <ninochka20102@gmail.com>
2023-01-26 00:55:17 +0500

    поиск, корзина, user

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from './cartSlice/reducer';
+import { userSlice } from './cartSlice/reduser';
 //import productSlise from './productSlise/reducer';
 
 export const store = configureStore({
 	reducer: {
 		//product: productSlise,
 		cart: cartSlice,
+		user: userSlice,
 	}
 })
